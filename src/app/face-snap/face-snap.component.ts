be7be7618a6476677bd5ed0b7b8c5f2154e46c5d
Raxuis <raclot.raphael@gmail.com>
2024-06-22 15:44:09 +0200

    feat(face snap component): :lipstick: improve ui

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -10,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class FaceSnapComponent implements OnInit {
   title!: string;
   description!: string;
-  created_at!: Date;
+  createdAt!: Date;
   snaps!: number;
   imageUrl!: string;
 
@@ -18,7 +18,7 @@ export class FaceSnapComponent implements OnInit {
     this.title = 'Coda';
     this.description = 'Where I\'m studying';
     this.imageUrl = 'https://static.wixstatic.com/media/324d06_440fbbb68a694e76884a751602f6c32c~mv2.jpg/v1/crop/x_20,y_35,w_925,h_338/fill/w_394,h_142,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/PROFILE-PIC-LK-3_edited.jpg';
-    this.created_at = new Date();
+    this.createdAt = new Date();
     this.snaps = 0;
   }
 }
