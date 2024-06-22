1dd0c070602e7ffff0a8a46e396cd9846217a40e
Raxuis <raclot.raphael@gmail.com>
2024-06-22 16:24:37 +0200

    feat(face snap component): :sparkles: made the component face snap dynamic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { FaceSnap } from './models/face-snap';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,16 @@ import { FaceSnapComponent } from './face-snap/face-snap.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  mySnap!: FaceSnap;
+
+  ngOnInit(): void {
+    this.mySnap = new FaceSnap(
+      'Coda',
+      'Where I\'m studying',
+      'https://static.wixstatic.com/media/324d06_440fbbb68a694e76884a751602f6c32c~mv2.jpg/v1/crop/x_20,y_35,w_925,h_338/fill/w_394,h_142,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/PROFILE-PIC-LK-3_edited.jpg',
+      0,
+      new Date()
+    )
+  }
+}
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -11,31 +11,21 @@ import { FaceSnap } from '../models/face-snap';
 export class FaceSnapComponent implements OnInit {
   @Input() faceSnap!: FaceSnap;
 
-  title!: string;
-  description!: string;
-  createdAt!: Date;
-  snaps!: number;
-  imageUrl!: string;
   snapButtonText!: string;
   hasSnapped!: boolean;
 
   ngOnInit(): void {
-    this.title = 'Coda';
-    this.description = 'Where I\'m studying';
-    this.imageUrl = 'https://static.wixstatic.com/media/324d06_440fbbb68a694e76884a751602f6c32c~mv2.jpg/v1/crop/x_20,y_35,w_925,h_338/fill/w_394,h_142,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/PROFILE-PIC-LK-3_edited.jpg';
-    this.createdAt = new Date();
-    this.snaps = 0;
     this.snapButtonText = 'Oh Snap!'
     this.hasSnapped = false;
   }
 
   onUserSnap(): void {
     if (this.hasSnapped) {
-      this.snaps--;
+      this.faceSnap.snaps--;
       this.hasSnapped = false;
       this.snapButtonText = 'Oh Snap!'
     } else {
-      this.snaps++;
+      this.faceSnap.snaps++;
       this.snapButtonText = 'Oops Unsnap!';
       this.hasSnapped = true;
     }
