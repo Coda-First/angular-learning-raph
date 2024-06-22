478709b527a64fac4680834e51f4ffa49188f331
Raxuis <raclot.raphael@gmail.com>
2024-06-22 15:06:57 +0200

    feat(face snap component): :sparkles: used attriattributing to use image url

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -12,10 +12,12 @@ export class FaceSnapComponent implements OnInit {
   description!: string;
   created_at!: Date;
   snaps!: number;
+  imageUrl!: string;
 
   ngOnInit(): void {
     this.title = 'Coda';
     this.description = 'Where I\'m studying';
+    this.imageUrl = 'https://static.wixstatic.com/media/324d06_440fbbb68a694e76884a751602f6c32c~mv2.jpg/v1/crop/x_20,y_35,w_925,h_338/fill/w_394,h_142,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/PROFILE-PIC-LK-3_edited.jpg';
     this.created_at = new Date();
     this.snaps = 0;
   }
