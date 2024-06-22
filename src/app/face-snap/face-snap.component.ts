c4788c7d38d56e5746c7046cc80c4f0f86e14bbe
Raxuis <raclot.raphael@gmail.com>
2024-06-22 14:58:50 +0200

    feat(face snap component): :sparkles: defined variables

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
@@ -7,6 +7,16 @@ import { Component } from '@angular/core';
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  created_at!: Date;
+  snaps!: number;
 
+  ngOnInit(): void {
+    this.title = 'Coda';
+    this.description = 'Where I\'m studying';
+    this.created_at = new Date();
+    this.snaps = 0;
+  }
 }
