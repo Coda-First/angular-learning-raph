5adefcfa7b29431de4e9338ec6ca887a68946979
Raxuis <raclot.raphael@gmail.com>
2024-07-01 12:16:48 +0200

    feat(header): :sparkles: implemented header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FaceSnapListComponent],
+  imports: [FaceSnapListComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
