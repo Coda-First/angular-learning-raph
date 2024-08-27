d341b5a4f2bda8d9879e9ab56116397c8fb4b20b
Raxuis <raclot.raphael@gmail.com>
2024-08-27 14:35:24 +0200

    feat(routes): :truck: add create route

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
 import { FaceSnapComponent } from '../face-snap/face-snap.component';
 import { FaceSnapsService } from '../services/face-snaps.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-face-snap-list',
   standalone: true,
-  imports: [FaceSnapComponent],
+  imports: [FaceSnapComponent, RouterLink],
   templateUrl: './face-snap-list.component.html',
   styleUrl: './face-snap-list.component.scss'
 })
