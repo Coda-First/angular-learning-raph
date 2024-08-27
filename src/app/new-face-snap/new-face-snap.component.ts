436e442612c2f5ffcc7400dab3afab3c83273490
Raxuis <raclot.raphael@gmail.com>
2024-08-27 14:44:09 +0200

    feat(create): :sparkles: add logic to create a faceSnap

--- src/app/new-face-snap/new-face-snap.component.ts
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FaceSnapsService } from '../services/face-snaps.service';
+import { FaceSnap } from '../models/face-snap';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-face-snap',
@@ -8,5 +11,23 @@ import { Component } from '@angular/core';
   styleUrl: './new-face-snap.component.scss'
 })
 export class NewFaceSnapComponent {
+  title: string = '';
+  description: string = '';
+  imageUrl: string = '';
+  location: string = '';
 
+  constructor(private faceSnapsService: FaceSnapsService, private router: Router) { }
+
+  onSubmit(): void {
+    const newFaceSnap = new FaceSnap(
+      this.title,
+      this.description,
+      this.imageUrl,
+      0,
+      new Date()
+    ).withLocation(this.location);
+
+    this.faceSnapsService.addFaceSnap(newFaceSnap);
+    this.router.navigateByUrl('/facesnaps'); // Redirect to the face snaps list
+  }
 }
